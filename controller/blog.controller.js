b73767ee588dd4a9d08825a92c7e1a9dc2263f15
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-12-09 21:44:02 +0330

    addind controllers methods

---
+++ controller/blog.controller.js
@@ -0,0 +1,56 @@
+async function getAllBlogs(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function createNewBlog(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function removeBlog(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function searchByTitle(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function searchByMultyFeild(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function searchByRegex(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports={
+    getAllBlogs,
+    createNewBlog,
+    removeBlog,
+    searchByTitle,
+    searchByMultyFeild,
+    searchByRegex
+}
---
+++ controller/indices.controller.js
@@ -0,0 +1,29 @@
+async function createIndex(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function removeIndex(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function getIndices(req, res, next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports= {
+    createIndex,
+    removeIndex,
+    getIndices
+}
