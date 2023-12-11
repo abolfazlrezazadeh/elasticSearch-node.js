b8af70a2951f7f7916ba41412c6b1d0608b67c14
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-12-11 16:37:53 +0330

    routing create new blog

--- router/router.js
+++ router/router.js
@@ -1,3 +1,4 @@
+const { blogRouter } = require("./blog.routes");
 const { indicesRoute } = require("./indices.routes");
 
 const allRouters = require("express").Router();
@@ -6,6 +7,7 @@ allRouters.get("/", (req, res, next) => {
   return res.render("pages/index");
 });
 allRouters.use("/index", indicesRoute)
+allRouters.use("/blog", blogRouter)
 
 module.exports = {
   allRouters,
--- validator/blog.validator.js
+++ validator/blog.validator.js
@@ -2,7 +2,8 @@ const joi = require("@hapi/joi");
 const createError = require("http-errors");
  const createBlogSchema = joi.object({
     title : joi.string().min(5).error(createError.BadRequest("title  must be at least 5 char")),
-    text : joi.string().min(10).error(createError.BadRequest("text must be at least 10 char"))
+    text : joi.string().min(10).error(createError.BadRequest("text must be at least 10 char")),
+    author : joi.string().min(3).error(createError.BadRequest("author must be at least 10 char")),
  })
 
  module.exports = {
