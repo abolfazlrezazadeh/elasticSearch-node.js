5b8fbfd74a6ef44a632f8ea7a1304d6959fa2890
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-12-14 11:30:03 +0330

    add CQRS achitecture

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -13,7 +13,16 @@ async function createNewBlog(req, res, next) {
   try {
     const blogBody = await createBlogSchema.validateAsync(req.body);
     const { title, author, text } = blogBody;
-    const createBlogResult = await blogModel.create({title , text, author})
+    const createBlogResult = await blogModel.create({title , text, author});
+    if(createBlogResult){
+      // we can do here some queries
+      // const finalData = await blogModel.aggregate([
+      //   {$match : {author : 1, title : 1 , text : 1, _id : 0}},
+      //   {$lookup : {}},
+      //   {$unwind : {}}
+      // ])
+      await saveToElastic(createBlogResult)
+    }
     return res.status(201).json({
       statusCode: 201,
       message: "blog created successfully",
@@ -22,6 +31,19 @@ async function createNewBlog(req, res, next) {
   } catch (error) {
     next(error);
   }
+}
+
+async function saveToElastic(data){
+  const createResult = await elasticClient.index({
+    index : indexName,
+    document : {
+      title : data.title,
+      text : data.text,
+      author : data.author
+    }
+  })
+  console.log(createResult);
+  if(createResult) return console.log("save to elastic is successfull")
 }
 
 async function removeBlog(req, res, next) {
