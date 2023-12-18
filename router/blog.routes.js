fb2a1bbc168e6164e3b6bce9feab837497683d82
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-12-18 10:51:07 +0330

    search by regexp and words in router side

--- router/blog.routes.js
+++ router/blog.routes.js
@@ -1,4 +1,4 @@
-const { createNewBlog, getAllBlogs, removeBlog, updateBlogInMongoDB, searchByTitle, searchByMultyFeild } = require("../controller/blog.controller");
+const { createNewBlog, getAllBlogs, removeBlog, updateBlogInMongoDB, searchByTitle, searchByMultyFeild, searchByRegexp } = require("../controller/blog.controller");
 
 const blogRouter = require("express").Router();
 
@@ -6,7 +6,9 @@ blogRouter.post("/create" , createNewBlog)
 // ? => is meaned the value is optional
 blogRouter.get("/list/:value?" , getAllBlogs)
 
-blogRouter.get("/multiFields?" , searchByMultyFeild)
+blogRouter.get("/multiFields" , searchByMultyFeild)
+
+blogRouter.get("/searchByRegexp" , searchByRegexp)
 
 blogRouter.get("/findByTitle" , searchByTitle)
 
